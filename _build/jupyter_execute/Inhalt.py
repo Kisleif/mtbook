e6854df169208807097e6dd9a77bac8e00697ddf
Katharina-Sophie Isleif <katharina-sophie.isleif@desy.de>
2022-05-09 14:37:46 +0200

    delete lecture

---
+++ _build/jupyter_execute/Inhalt.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# diesen Code durch Eingabe von <shift><ret> ausführen
+# Für diese Notebook benötigte Pakete:
+from IPython.display import Image
+from IPython.core.display import HTML 
+
+
+# ---
+# #  Vorlesungsskript zur Messtechnik
+# 
+# ##### Helmut-Schmidt-Universität, Universität der Bundeswehr Hamburg
+# 
+# Dr. rer. nat. Katharina-Sophie Isleif, isleifk@hsu-hh.de
+# 
+
+# ## Jupyter Notebooks
+# Dieses github-Repository beinhaltet eine Anzahl von [Jupyter](https://jupyter.org/) Notebooks, welche Notizen und Beispiele für das Bachelormodul Messtechnik an der Helmut-Schmidt-Universität Hamburg, Universität der Bundeswehr, gelesen von Katharina-Sophie Isleif, bereitstellt. Das Notebook beinhaltet Beispiele in der Programmiersprache [IPython 3](http://ipython.org/), welche auch interaktiv ausgeführt werden können. Eine kurze Anleitung zur Ausführung von Blocks und Code in Jupyter Notebooks kann im [Jupyter Notebook Tutorial](00_jupyter_notebook_tutorial.ipynb) gefunden werden. 
+# 
+# - [**Interaktive Kursmaterialien starten**](https://mybinder.org/v2/gh/Kisleif/messtechnik-lecture/HEAD?filepath=00_Inhalt.ipynb)
+# - [Kursmaterialien nur lesen](https://nbviewer.jupyter.org/github/Kisleif/messtechnik-lecture/blob/main/00_Inhalt.ipynb)
+# - [Installationshinweise & Grundlagen der Programmierung in Python (Github von Nils Fleischer für einen anderen Kurs)](https://nbviewer.jupyter.org/github/nilsleiffischer/python-course/blob/master/setup.ipynb)
+
+# --- 
+
+# In[2]:
+
+
+Image("pictures/mt.png", width=1000)
+
+
+# # Inhalt (vorläufig)
+# 
+# Der Inhalt dieses Vorlesungsskripts wird im laufenden Trimester entwickelt und erweitert. Gerne können Anmerkungen zu Fehlern oder Anregungen gemeldet werden, beispielsweise als [issues](https://github.com/Kisleif/messtechnik-lecture/issues) auf GitHub oder über direkte Kontaktaufnahme über isleifk@hsu-hh.de.
+
+# ***
+# ## Teil 1: Grundlagen zu Messsystemen, -signalen, -abweichungen und Auswertung
+
+# * [Einleitung: Einheiten, Kalibrieren und Eichen](Einleitung.ipynb)
+# * [Stationäre Messsysteme](Messsysteme.ipynb)
+# * [Messabweichungen](Messabweichungen.ipynb)
+# * [Kurvenanpassung](Kurvenanpassung.ipynb)
+# * [Messsignale](Messsignale.ipynb)
+# * [Dynamische Messsysteme](Dynamische_Messsysteme.ipynb)
+# * Messen elektrischer Größen 
+
+# ***
+# ## Teil 2: Sensoren
+
+# * Auswahl an Sensoren
+
+# # Literatur
+# 
+# * Parthier, R. (2020). Messtechnik: Vom SI-Einheitensystem über Bewertung von Messergebnissen zu Anwendungen der elektrischen Messtechnik (9th 2020 ed.). Wiesbaden: Springer Vieweg
+# * Böttcher, J. (2020). Kompendium Messtechnik und Sensorik: https://www.bod.de/buchshop/kompendium-messtechnik-und-sensorik-joerg-boettcher-9783752632491, www.unibw.de/regelungs-und-messtechnik
+# * Weiterführende Literatur: Puente León, F. (2019). Messtechnik: Grundlagen, Methoden und Anwendungen (11. Auflage). Springer Vieweg
+# * Weiterführende Literatur: Brian Douglas, Fundamentals of Control Theory, https://engineeringmedia.com
+
+# # Beitragende
+# 
+# * Katharina-Sophie Isleif (Autor)

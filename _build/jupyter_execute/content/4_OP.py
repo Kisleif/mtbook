eb6835e41cfc461624b18183e13dff69eb1ae923
Katharina-Sophie Isleif <katharina-sophie.isleif@desy.de>
2022-11-29 14:28:42 +0100

    OP youtube embed

--- _build/jupyter_execute/content/4_OP.py
+++ _build/jupyter_execute/content/4_OP.py
@@ -1,18 +1,35 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Operationsverstärker
+# # Operationsverstärker (OP)
 
-# # Beispiel Operationsverstärkerschaltungen:
-
-# <iframe width="560" height="315" src="https://www.youtube.com/embed/wiq93KjXwao" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+# ## Einführung
 
 # ::::::{margin}
 # :::::{grid}
 # ::::{grid-item-card}
-# :link: https://www.youtube.com/embed/wiq93KjXwao
+# :class-header: bg-light
+# Operationsverstärker
 # 
-# <iframe width="560" height="315" src="https://www.youtube.com/embed/wiq93KjXwao" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+# <iframe width="220" src="https://www.youtube.com/embed/nl3JZbj9kHs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 # ::::
 # :::::
 # ::::::
+
+# Liegen kleine Signale vor, wie z.B. kleine Stromstärken $I$,  Spannungswerte $U$ oder Widerstandswerte $R$, so werden die Signale häufig als erstes verstärkt. Dies sollte möglichst **rückwirkungsfrei** geschehen, das heißt dass die Verstärkerschaltung möglichst wenig Einfluss auf die Messgröße und die weiterführende Signalverarbeitung hat. 
+# Für eine Spannung, die man anlegt, sollte der Innenwiderstand also möglichst groß sein, um den kompletten Spannungsabfall zu messen. 
+# Desweiteren möchte man eine **hohe Empfindlichkeit** und eine **große Verstärkung** erreichen, weshalb eine zusätzliche Strom- bzw. Spannungsversorgung von Nöten ist, da die zusätzlich Energie von Außen dem System zugeführt werden muss. Dies ist der allgemein Sinn von Operationsverstärkern. 
+# Das Ausgangssignal sollte weiterdessen direkt mit dem Eingangssignal korreliert sein, d.h. wir wünschen uns ein **definiertes Übertragungsverhalten**. Dem Eingangssignal sollte zudem möglichst gut und schnell gefolgt werden, d.h. es sollten keine allzu großen Verzögerungen auftreten. Dies wird über das **dynamische Verhalten** des verwendeten OPs bestimmt. 
+# Am Ausgang sollte ein möglichst kleiner Innenwiderstand vorzufinden sein, sodass wir ein **eingeprägtes Ausgangssignal** annehmen können, d.h. das Ausgangssignal ist gut weiterverwendbar und beeinflusst nicht die folgende Schaltung. 
+
+# ::::::{margin}
+# :::::{grid}
+# ::::{grid-item-card}
+# :class-header: bg-light
+# Elektrometer und weitere Beispiele
+# 
+# <iframe width="220" src="https://www.youtube.com/embed/wiq93KjXwao" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+# ::::
+# :::::
+# ::::::
+# 

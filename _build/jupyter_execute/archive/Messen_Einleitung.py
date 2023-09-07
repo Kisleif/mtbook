1cf9906df2d6ff82b5d2a8b63956ace15a9bf07a
Katharina-Sophie Isleif <katharina-sophie.isleif@desy.de>
2023-09-07 11:23:07 +0200

    update structure

---
+++ _build/jupyter_execute/archive/Messen_Einleitung.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Messen von Zeit, Länge, Masse, Strom, Temperatur, Lichtstärke und Stoffmenge
+# 
+# **Aufgabe:** Geben Sie für die physikalischen Größen (Zeit, Länge, Masse, Strom, Temperatur, Lichtstärkemessung) eine Auswahl an Messgeräten an und erläutern Sie kurz ein von Ihnen ausgewähltes Messprinzip etwas genauer. Orientieren Sie sich gerne an folgenden Fragen:
+# - Wie können (Zeit, Länge, Masse, …) gemessen werden? Heute vs. Früher? 
+# - Sind Ihnen Vor- und Nachteile bei dem Vergleich verschiedener Messverfahren aufgefallen (Anwendungsbereich, Nutzer, Messgenauigkeit,…)?
+# - In welchen Einheiten wird (Zeit, Länge, Masse, …) noch angegeben (Meile, Lichtjahr, Minuten, Fahrenheit, …)?
+# - Andere interessante Fakten
+# 
+# ## Aufgabe 1: Zeitmessung
+# … liefert die Angabe von Messgrößen der Zeit und wird in SI-Einheiten in Sekunden angegeben. Sie umfasst sowohl die Bestimmung von Zeitpunkten, als auch von Zeitintervallen und ist in unzähligen Anwendungsbereichen essentieller Bestandteil, wie z.B. in der Messtechnik, Astronomie, Geodäsie, Kinematik, GPS, Sport. 
+# 
+# ## Aufgabe 2: Längenmessung
+# … ist die Messung des Abstands zwischen zwei Punkten im Raum. Sie umfasst sowohl die Bestimmung des Absolutabstands, als auch der inkrementalen Längenänderung. Der physikalisch relevante Bereich beginnt bei  (Größe der Elementarteilchen) und reicht bis  (Größe des Universums). 
+# 
+# ## Aufgabe 3: Massenmessung
+# … liefert eine der wichtigsten Eigenschaften von Materie. Die Masse  eines Körpers ist träg (2. Newton’sche Axiom  ) und schwer (Newtons Gravitationsgesetz ). Masse ist proportional zur Kraft, welche einerseits auf den Körper wirkt, als auch von ihm verursacht wird. Auf diesem Prinzip funktionieren Waagen. 
+# 
+# ## Aufgabe 4: Strommessung
+# … liefert die Angabe über elektrische Stromstärken. In der elektrischen Messtechnik ist der Stromfluss einer der am häufigsten verwendeten Parameter um ein elektronisches System zu charakterisieren, steuern oder dessen Effektivität zu bestimmen. 
+# 
+# ## Aufgabe 5: Temperaturmessung
+# … liefert den Wert eine Zustandsgröße von zentraler Bedeutung bei der makroskopischen Betrachtung von physikalischen und chemischen Zuständen und diversen Prozessen in Wissenschaft, Technik und Umwelt. Mikroskopisch betrachtet entspricht eine Erhöhung Temperatur einer Zunahme der durchschnittlichen Energie von sich bewegenden Teilchen.  Dies ist auch bekannt als statistische Physik. 
+# 
+# ## Aufgabe 6: Lichtstärkemessung
+# … liefert den auf einen Raumwinkel bezogenen Lichtstrom und beschreibt somit eine Eigenschaft der betrachteten Lichtquelle. Die Lichtstärke ist eine photometrische Größe und bezieht aber auch die Empfindlichkeit des menschlichen Auges mit ein. 
+# 
+# ## Aufgabe 7: Extra Ü: Stoffmengenmessung
+# … ist die Bestimmung von Teilchenanzahlen, wie Molekülen oder Ionen, die man nicht mehr zählen kann. Der Fachbegriff in der Chemie lautet Stöchiometrie und befasst sich mit der Stoffmengenmessung über Massen-Messungen. Im Alltag oder der Umwelt ist diese Messtechnik von Nutzen, wenn z.B. die Menge von ausgestoßenen Stickoxiden eines Fahrzeugs bestimmt werden soll.
+# 
+
+# In[ ]:
+
+
+
+
